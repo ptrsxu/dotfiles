910d4c0b66a55c9fd32732aaaf7a0b8f74c3b6f2
Peter Xu <p@cooliktas.com>
2014-04-18 15:41:09 +0800

    update ipfu.sh, add some build tools.

--- ipfu.sh
+++ ipfu.sh
@@ -8,10 +8,12 @@ apt-get upgrade --yes
 ################################################################################
 # setup devel environment
 ################################################################################
-apt-get install --yes vim ctags cscope
+apt-get install --yes vim-gnome ctags cscope global
 apt-get install --yes git tig gitolite mercurial subversion
+apt-get install --yes ack-grep
 
-apt-get install --yes automake autotools-dev autoconf
+apt-get install --yes automake autotools-dev autoconf libtool build-essential
+apt-get install --yes clang cmake
 
 # for web
 apt-get install --yes nginx curl traceroute ifstat nethogs
@@ -19,18 +21,17 @@ apt-get install --yes nginx curl traceroute ifstat nethogs
 # for better shell ops
 apt-get install --yes autojump
 apt-get install --yes powerline
+apt-get install --yes xclip
 
 # for some lib
 apt-get install --yes libyaml-dev
+apt-get install --yes libargtable2-dev  # for gnu arg parse
+
 # python pillow will need them for png/jpeg/... support
 apt-get install --yes libjpeg-dev libfreetype6 libfreetype6-dev zlib1g-dev
 
-# for database, a password is required for mysql-server during the installation.
-apt-get install --yes postgresql mysql-client libmysqlclient-dev mysql-server
-apt-get install --yes memcached libmemcached-dev redis-server
-
 # for python2
-apt-get install --yes ipython python-numpy python-scipy python-matplotlib 
+apt-get install --yes python-dev ipython python-numpy python-scipy python-matplotlib
 apt-get install --yes python-pip
 apt-get install --yes python-virtualenv
 apt-get install --yes python-sphinx
@@ -39,7 +40,7 @@ apt-get install --yes python-flask
 apt-get install --yes vim-python
 
 # for python3
-apt-get install --yes ipython3 python3-pip
+apt-get install --yes python3-dev ipython3 python3-pip
 
 # for python-recsys
 #apt-get install csc-pysparse
@@ -50,3 +51,7 @@ apt-get install --yes ipython3 python3-pip
 
 # for python editor eric
 apt-get install --yes eric
+
+# for database, a password is required for mysql-server during the installation.
+apt-get install --yes memcached libmemcached-dev redis-server
+apt-get install --yes postgresql mysql-client libmysqlclient-dev mysql-server
