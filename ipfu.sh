14ed2f553a25f80feb6eafbed998433f78022eba
Peter Xu <ptrsxu@users.noreply.github.com>
2024-11-06 15:51:43 +0800

    Update ipfu.sh

--- ipfu.sh
+++ ipfu.sh
@@ -197,9 +197,9 @@ curl https://raw.githubusercontent.com/rcaloras/bash-preexec/master/bash-preexec
 # for details
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/JetBrains/JetBrainsMono/master/install_manual.sh)"
 
-# for node v20
+# for node v22
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
+NODE_MAJOR=22
 echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
 apt-get update && apt-get install --yes nodejs
 
